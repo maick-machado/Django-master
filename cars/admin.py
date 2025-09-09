b01058bcf1e0a875d4d13ac815b2563fc55c18a0
maick-machado <maick.armachado@gmail.com>
2025-09-08 22:14:33 -0400

    admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from cars.models import Cars, Brand
 
-# Register your models here.
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name')
+    search_fields = ('name')
+    
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'year', 'value')
+    search_fields = ('model',)
+
+admin.site.register(model_or_iterable=Brand, admin_class=BrandAdmin)
+admin.site.register(model_or_iterable=Cars, admin_class=CarAdmin)
--- cars/models.py
+++ cars/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 
-class Cars(models.Model):
+class Brand(models.Model):
+
     id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.model
+
+class Cars(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
+    year = models.IntegerField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    photos = models.ImageField(upload_to='cars/', blank=True, null=True)
+
+    def __str__(self):
+        return self.model
