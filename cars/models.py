9f6d6fca708fd1cb138811fdd6adf706abbed36b
maick-machado <maick.armachado@gmail.com>
2025-09-30 14:02:33 -0400

    created an inventory management

--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Brand(models.Model):
 
     id = models.AutoField(primary_key=True)
@@ -8,20 +9,35 @@ class Brand(models.Model):
     def __str__(self):
         return self.name
 
+
 class Car(models.Model):
 
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name="car_brand")
     year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
     # quando eu deleto o carro do banco de dados a foto permanece na pasta do projeto
     photos = models.ImageField(
-        default='cars/sem_imagem.png',
-        upload_to='cars/', 
-        blank=True, 
-        null=True,)
-
+        default="cars/sem_imagem.png",
+        upload_to="cars/",
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return self.model
+
+
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = [
+            "-created_at"
+        ]  # o sinal "-" ordena do mais recente para o mais antigo
+
+    def __str__(self):
+        return f"Inventory at {self.created_at}: {self.cars_count} cars worth {self.cars_value}"
--- cars/signals.py
+++ cars/signals.py
@@ -1,24 +1,33 @@
 from django.db.models.signals import pre_save, post_save, pre_delete, post_delete
+from django.db.models import Sum
 from django.dispatch import receiver
-from cars.models import Car
+from cars.models import Car, CarInventory
 
 
-@receiver(pre_save, sender="cars.Car")
-def car_pre_save(sender, instance, **kwargs):
-    # possivel usar para enviar email assim que for cadastrado ou deletado
-    print(f"Pre-save signal triggered for Car: {instance}")
+# @receiver(pre_save, sender="cars.Car")
+# def car_pre_save(sender, instance, **kwargs):
+#     # possivel usar para enviar email assim que for cadastrado ou deletado
+#     print(f"Pre-save signal triggered for Car: {instance}")
+def car_invetory_update():
+    cars_count = Car.objects.all().count()
+    # cars_value = sum(car.value for car in Car.objects.all() if car.value)
+    cars_value = Car.objects.aggregate(total_valeu=Sum("value"))[
+        "total_valeu"
+    ]  # como retorna um dicionário, usa-se o ["total_value"] para acessar somente o valor
+    CarInventory.objects.create(cars_count=cars_count, cars_value=cars_value)
 
 
 @receiver(post_save, sender="cars.Car")
 def car_post_save(sender, instance, **kwargs):
-    print(f"Post-save signal triggered for Car: {instance}")
+    # if created: da para adicionar o if created e adicionar o created nos parametros da função para diferenciar se é criação ou atualização
+    car_invetory_update()
 
 
-@receiver(pre_delete, sender="cars.Car")
-def car_pre_delete(sender, instance, **kwargs):
-    print(f"Pre-delete signal triggered for Car: {instance}")
+# @receiver(pre_delete, sender="cars.Car")
+# def car_pre_delete(sender, instance, **kwargs):
+#     print(f"Pre-delete signal triggered for Car: {instance}")
 
 
 @receiver(post_delete, sender="cars.Car")
 def car_post_delete(sender, instance, **kwargs):
-    print(f"Post-delete signal triggered for Car: {instance}")
+    car_invetory_update()
